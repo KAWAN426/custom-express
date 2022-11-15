ba14ebb612a6a62393d43936ff39400feb1f7ddb
CWIN <81596708+CWIN77@users.noreply.github.com>
2022-11-15 11:16:23 +0900

    Delete app directory

--- app/src/routes/home/home.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express();
-
-const {output,process} = require("./home.ctrl");
-
-router.post("/", process.home);
-router.get("/", output.home);
-
-module.exports = router;
