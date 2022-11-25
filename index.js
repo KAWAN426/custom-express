6f632fb2d29b973826749f8a0b5cd13ce38c142a
CWIN <81596708+CWIN77@users.noreply.github.com>
2022-11-25 17:50:45 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,6 @@
-/** With Cluster */
-const cluster = require('cluster');
-const totalCPUs = require('os').cpus().length;
+const app = require("./src/app");
+const PORT = process.env.PORT || 8080;
 
-if (cluster.isMaster) {
-  console.log(`Master ${process.pid} is running`);
-  for (let i = 0; i < totalCPUs; i++) {
-    cluster.fork();
-  }
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`${worker.process.pid}가 종료됐습니다.`);
-    cluster.fork();
-  });
-} else {
-  const app = require("./src/app");
-  const PORT = process.env.PORT || 9000;
-
-  console.log(`Worker ${process.pid} started`);
-  app.listen(PORT, () => {
-    console.log(`Server on : http://localhost:${PORT}`);
-  });
-}
-
-
-/** Without Cluster */
-// const app = require("./src/app");
-// const PORT = process.env.PORT || 8080;
-
-// // console.log(`Worker ${process.pid} started`);
-// app.listen(PORT, () => {
-//   console.log(`Server on : http://localhost:${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server on : http://localhost:${PORT}`);
+});
