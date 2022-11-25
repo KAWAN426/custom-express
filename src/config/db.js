5cd4c2a9ca590c13f0cde56de06ba2dbec7bf33a
CWIN <81596708+CWIN77@users.noreply.github.com>
2022-11-25 18:01:37 +0900

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,15 @@
 const mongoose = require("mongoose");
+
+let isConnected = 0;
+
 const connectDB = async () => {
+  if (isConnected === 1) return;
   try {
     const db = await mongoose.connect(process.env.MONGODB_URI);
+    isConnected = db.connections[0].readyState;
     console.log(`MongoDB Connected: ${db.connection.host}`);
   } catch (err) {
     console.log(err);
-    process.exit(1);
   }
 }
 
